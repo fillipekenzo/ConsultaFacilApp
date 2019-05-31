593e67c86b40c6ed0522786f04aec7b5626b1d9f
Vitor <vitor17martins@hotmail.com>
2019-05-31 18:09:13 -0400

    DataPicker

--- CFApp/CFApp/CFApp/Home.xaml.cs
+++ CFApp/CFApp/CFApp/Home.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Forms.Maps;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,20 @@ namespace CFApp
 		public Home ()
 		{
 			InitializeComponent ();
-		}
+
+            Mapa.MoveToRegion(MapSpan.FromCenterAndRadius(
+                             new Position(-23.4859591, -47.4420192),
+                             Distance.FromMiles(0.5)));
+
+            var pin = new Pin
+            {
+                Type = PinType.Place,
+                Position = new Position(-23.4859591, -47.4420192),
+                Label = "Demo Maps",
+                Address = "www.julianocustodio.com",
+            };
+
+            Mapa.Pins.Add(pin);
+        }
 	}
 }
--- CFApp/CFApp/CFApp/MarcarConsulta.xaml.cs
+++ CFApp/CFApp/CFApp/MarcarConsulta.xaml.cs
@@ -14,7 +14,9 @@ namespace CFApp
 	{
 		public MarcarConsulta ()
 		{
+
 			InitializeComponent ();
 		}
+
 	}
 }
