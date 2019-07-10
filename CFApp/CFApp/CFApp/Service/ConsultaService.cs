69e3a45ac09375425057a89736d4d6e3efe7b373
Fabrica <Fabrica@CGR-24019.senacmsedu.local>
2019-07-10 16:59:49 -0400

    Api implementacao

---
+++ CFApp/CFApp/CFApp/Service/ConsultaService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace CFApp.Service
+{
+    public class ConsultaService
+    {
+        
+        private readonly HttpClient _client;
+        public ConsultaService()
+        {
+            var uri = "";
+            _client = new HttpClient();
+           
+        }
+        public async void CadastrarConsulta()
+        {
+            var response = await _client.GetAsync(uri);
+        }
+
+            
+
+
+    }
+}
